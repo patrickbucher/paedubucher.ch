37628cbe0603fde20a8ef90341a2a381ac7bda65
Patrick Bucher <patrick.bucher@mailbox.org>
2024-02-14 09:39:46 +0100

    separate YAML front matter from Markdown content

--- gengo/main.go
+++ gengo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -38,7 +39,7 @@ func main() {
 	baseDir := os.Args[1]
 	stat, err := os.Stat(baseDir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s does not exist\n", baseDir)
+		fmt.Fprintf(os.Stderr, "stat %s: %v\n", baseDir, err)
 		os.Exit(1)
 	}
 	if !stat.IsDir() {
@@ -46,11 +47,81 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, _, err = scaffold(baseDir)
+	articlesMdDir := joinPath(baseDir, "articles.md")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "stat %s: %v\n", articlesMdDir, err)
+		os.Exit(1)
+	}
+	if !stat.IsDir() {
+		fmt.Fprintf(os.Stderr, "%s is not a directory\n", baseDir)
+		os.Exit(1)
+	}
+
+	_, articlesDir, err := scaffold(baseDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "scaffolding: %v\n", err)
 		os.Exit(1)
 	}
+
+	fmt.Println(articlesMdDir, articlesDir)
+
+	parseArticles(articlesMdDir, articlesDir)
+}
+
+func parseArticles(sourceDir, targetDir string) ([]*Article, error) {
+	articles := make([]*Article, 0)
+	entries, _ := os.ReadDir(sourceDir)
+	for _, entry := range entries {
+		articleSrcPath := joinPath(sourceDir, entry.Name())
+		article, err := parseArticle(articleSrcPath)
+		if err != nil {
+			return articles, err
+		}
+		articles = append(articles, article)
+	}
+
+	return articles, nil
+}
+
+func parseArticle(path string) (*Article, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("open article %s: %v", path, err)
+	}
+	data, err := io.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("read from %s: %v", path, err)
+	}
+	content := string(data)
+	yaml, md, err := splitParts(content)
+	if err != nil {
+		return nil, fmt.Errorf("parse article %s: %v", path, err)
+	}
+	fmt.Println(yaml, md[:100])
+
+	return nil, nil
+}
+
+func splitParts(content string) (string, string, error) {
+	const sep = "---"
+	var yaml, md bytes.Buffer
+	inYaml, inMd := false, false
+	lines := strings.Split(content, "\n")
+	for _, line := range lines {
+		if strings.TrimSpace(line) == sep {
+			if inYaml {
+				inYaml = false
+				inMd = true
+			} else if yaml.Len() == 0 {
+				inYaml = true
+			}
+		} else if inYaml {
+			yaml.WriteString(line + "\n")
+		} else if inMd {
+			md.WriteString(line + "\n")
+		}
+	}
+	return yaml.String(), md.String(), nil
 }
 
 func scaffold(baseDir string) (string, string, error) {
@@ -61,14 +132,12 @@ func scaffold(baseDir string) (string, string, error) {
 	}
 	os.Mkdir(publicDir, 0750)
 
-	articleDir := joinPath(baseDir, "articles.md")
+	articleDir := joinPath(publicDir, "articles")
 	stat, err = os.Stat(articleDir)
-	if err != nil {
-		return "", "", fmt.Errorf("stat %s: %v", articleDir, err)
+	if err == nil && stat.IsDir() {
+		os.RemoveAll(articleDir)
 	}
-	if !stat.IsDir() {
-		return "", "", fmt.Errorf("%s is not a directory", articleDir)
-	}
+	os.Mkdir(publicDir, 0750)
 
 	staticDir := strings.Join([]string{baseDir, "static"}, string(os.PathSeparator))
 	for _, entry := range staticAssets {
